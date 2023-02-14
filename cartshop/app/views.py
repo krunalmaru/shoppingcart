0636bcedd920a200a086c9635da0f1babfa59fd8
krunal <krunal.maru1106@gmail.com>
2023-02-14 18:23:08 +0530

    render all product and view product details by id

--- cartshop/app/views.py
+++ cartshop/app/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from .models import Product,Cart,Customer,OrderPlaced
+from django.views import View
+# def home(request):
+#  return render(request, 'app/home.html')
 
-def home(request):
- return render(request, 'app/home.html')
+class ProductView(View):
+    def get(self,request):
+        topwear = Product.objects.filter(category= 'TW')
+        bottomwear = Product.objects.filter(category= 'BW')
+        mobile = Product.objects.filter(category= 'M')
+        laptop = Product.objects.filter(category= 'L')
+        context = {'topwear':topwear,'bottomwear':bottomwear,'mobile':mobile,'laptop':laptop}
+        return render(request, 'app/home.html',context)
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+class ProductDetailView(View):
+    def get(self,request, pk):
+        productdetail = Product.objects.get(pk=pk)
+        context = {'productdetail':productdetail}
+        return render(request,'app/productdetail.html',context)
+        
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
